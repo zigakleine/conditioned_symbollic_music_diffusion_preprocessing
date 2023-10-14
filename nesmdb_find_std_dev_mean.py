29ac4ca1e8f44228a6a1e1e1b5bd0ffe72d19d5c
zigakleine <ziga.kleine@gmail.com>
2023-10-14 19:32:45 +0200

    hhh

--- nesmdb_find_std_dev_mean.py
+++ nesmdb_find_std_dev_mean.py
@@ -59,7 +59,7 @@ for game in metadata:
                     in_range = np.logical_and(lt1, m1)
                     in_range_count = np.count_nonzero(in_range)
                     total_elements = in_range.size
-                    in_range_perc = in_range/total_elements
+                    in_range_perc = in_range_count/total_elements
 
                     seq_ = (seq - data_min) / (data_max - data_min)
                     seq_ = 2. * seq_ - 1.
@@ -69,7 +69,7 @@ for game in metadata:
                     in_range_ = np.logical_and(lt1_, m1_)
                     in_range_count_ = np.count_nonzero(in_range_)
                     total_elements_ = in_range_.size
-                    in_range_perc_ = in_range_/total_elements_
+                    in_range_perc_ = in_range_count_/total_elements_
 
                     print(f"in-range-before-{in_range_perc} in-range-after-{in_range_perc_}")
 
